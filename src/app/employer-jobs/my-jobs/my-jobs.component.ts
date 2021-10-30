8e8f2a491dd887255d85cab8b6f44de6482036af
Péter Erdősi <peter.erdosi@extremalelement.com>
2021-10-30 14:32:12 +0200

    basic layout for my jobs are implemented

--- src/app/employer-jobs/my-jobs/my-jobs.component.ts
+++ src/app/employer-jobs/my-jobs/my-jobs.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { forkJoin } from 'rxjs';
+import { Job } from 'src/app/interfaces/job';
+import { DataService } from 'src/app/services/data.service';
+import { LanguageService } from 'src/app/services/language.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-my-jobs',
@@ -6,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-jobs.component.scss']
 })
 export class MyJobsComponent implements OnInit {
-
-  constructor() { }
+  myJobsTitleText: string = '';
+  myJobs: Job[] = new Array();
+  constructor(private dataService: DataService, private languageService: LanguageService) { }
 
   ngOnInit(): void {
+    forkJoin([
+      this.dataService.getAllData('/api/jobs/public/getJobsByToken', this.dataService.getAuthHeader()),
+      this.dataService.getAllData('/api/publicContents/getByPagePlaceKey/employerJobs/public')
+    ]).subscribe(res => {
+      console.log(res);
+      this.myJobs = res[0];
+      this.languageService.languageObservable$.subscribe(lang => {
+        this.myJobs = this.myJobs.map(element => {
+          element.logoUrl = environment.apiDomain + '/' + element.logoUrl;
+          element.selectedTranslation = this.languageService.getTranslation(lang, element.JobTranslations);
+          return element;
+        });
+        this.myJobsTitleText = this.languageService.getTranslationByKey(lang, res[1], 'title', 'myJobsTitleText', 'PublicContentTranslations');
+      });
+    });
   }
 
 }

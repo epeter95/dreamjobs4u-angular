686654e5b7e653430db80d07daba794ba22cf01c
Péter Erdősi <peter.erdosi@extremalelement.com>
2021-10-31 18:36:07 +0100

    home component search box design is initialized

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Category } from '../interfaces/category';
+import { FormElement } from '../interfaces/form-element';
+import { DataService } from '../services/data.service';
+import { LanguageService } from '../services/language.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +11,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  searchTermElement: FormElement = {
+    key: 'searchTerm', placeholder: '', focus: false,
+  };
 
-  constructor() { }
+  categoriesDropDown: FormElement = {
+    key: 'category', placeholder: '', focus: false,
+  };
+
+  categories: Category[] = new Array();
+  isCategoryDropdownOpen: boolean = false;
+
+  searchForm: FormGroup = new FormGroup({
+    searchTerm: new FormControl(''),
+    category: new FormControl(''),
+  });
+  constructor(private dataService: DataService,
+    private languageService: LanguageService) { }
 
   ngOnInit(): void {
+    this.dataService.getAllData('/api/categories/public').subscribe(res=>{
+      this.languageService.languageObservable$.subscribe(lang=>{
+        this.categories = res.map((element: Category)=>{
+          element.selectedTranslation = this.languageService.getTranslation(lang, element.CategoryTranslations);
+          return element;
+        });
+      });
+    })
+  }
+
+  setSelectedCategory(category: Category){
+    this.searchForm.controls.category.setValue(category.selectedTranslation.text);
+    this.isCategoryDropdownOpen = false;
+  }
+
+  openCategory(){
+    this.isCategoryDropdownOpen = !this.isCategoryDropdownOpen;
   }
 
 }

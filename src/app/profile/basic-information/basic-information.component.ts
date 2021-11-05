e52c1b63caef224645840cc1888722237c2b8982
Péter Erdősi <peter.erdosi@extremalelement.com>
2021-11-05 18:47:32 +0100

    upload cv is implemented

--- src/app/profile/basic-information/basic-information.component.ts
+++ src/app/profile/basic-information/basic-information.component.ts
@@ -29,6 +29,10 @@ export class BasicInformationComponent implements OnInit, OnDestroy {
   isEmployerRole: boolean = false;
   isEmployeeRole: boolean = false;
   generalMessages: PublicContent[] = new Array();
+  cvUrl: any = '';
+  cvUploadUrl: any = '';
+  cvChanging: boolean = false;
+  fileData!: File | string;
 
   basicInfoFormElements: FormElement[] = [
     { key: 'profileFirstName', placeholder: '', focus: false, profileDataKey: 'firstName' },
@@ -37,6 +41,7 @@ export class BasicInformationComponent implements OnInit, OnDestroy {
     { key: 'profileAge', placeholder: '', focus: false, profileDataKey: 'age' },
     { key: 'profileCurrentSalary', placeholder: '', focus: false, profileDataKey: 'currentSalary', roleName: 'employee' },
     { key: 'profileExpectedSalary', placeholder: '', focus: false, profileDataKey: 'expectedSalary', roleName: 'employee' },
+    { key: 'profileCv', placeholder: '', focus: false, profileDataKey: 'cvPath', fieldType: 'file' },
     { key: 'profileDescription', placeholder: '', focus: false, fieldType: 'textarea', profileDataKey: 'description' }
   ];
 
@@ -45,6 +50,7 @@ export class BasicInformationComponent implements OnInit, OnDestroy {
     profileLastName: new FormControl(''),
     profileJobTitle: new FormControl(''),
     profileAge: new FormControl(''),
+    profileCv: new FormControl(''),
     profileCurrentSalary: new FormControl(''),
     profileExpectedSalary: new FormControl(''),
     profileDescription: new FormControl(''),
@@ -72,6 +78,8 @@ export class BasicInformationComponent implements OnInit, OnDestroy {
         this.profileData = res[0];
         this.publicContents =res[1];
         this.generalMessages = res[2];
+        console.log(this.profileData.Profile.cvPath.substr(this.profileData.Profile.cvPath.lastIndexOf("/") + 1));
+        this.cvUrl = this.profileData.Profile.cvPath.substr(this.profileData.Profile.cvPath.lastIndexOf("/") + 1);
         this.languageSubscription = this.languageService.languageObservable$.subscribe(lang => {
           if(lang){
             this.basicInfoTitleText = this.languageService.getTranslationByKey(lang, this.publicContents, 'title', 'profileBasicInfoTitle', 'PublicContentTranslations');
@@ -93,22 +101,45 @@ export class BasicInformationComponent implements OnInit, OnDestroy {
     });
   }
 
+  handleCVPdf(event: any){
+    this.fileData = event.target.files[0] as File;
+    console.log(event.target.files[0].name);
+    this.cvUrl = this.fileData.name;
+    this.basicInfoForm.controls.profileCv.setValue('value');
+    this.cvChanging = true;
+    const files = event.target.files;
+    const reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.cvUploadUrl = reader.result;
+    }
+  }
+
   saveInfos() {
     let userResult = {
       firstName: this.basicInfoForm.controls.profileFirstName.value,
       lastName: this.basicInfoForm.controls.profileLastName.value
     }
-    let profileResult = {
+    let profileResult: any = {
       jobTitle: this.basicInfoForm.controls.profileJobTitle.value,
       age: this.basicInfoForm.controls.profileAge.value,
       currentSalary: this.basicInfoForm.controls.profileCurrentSalary.value,
       expectedSalary: this.basicInfoForm.controls.profileExpectedSalary.value,
-      description: this.basicInfoForm.controls.profileDescription.value
+      description: this.basicInfoForm.controls.profileDescription.value,
+    }
+    let profileFormData = new FormData();
+    const attributes = Object.keys(profileResult);
+    for (let i = 0; i < attributes.length; ++i) {
+      profileFormData.append(attributes[i], profileResult[attributes[i]]);
+    }
+    if (this.cvChanging) {
+      profileFormData.append('cvPath', this.fileData);
     }
     forkJoin([
       this.dataService.httpPostMethod('/api/users/public/modifyUserData', userResult, this.dataService.getAuthHeader()),
-      this.dataService.httpPostMethod('/api/profiles/public/modifyProfileData', profileResult, this.dataService.getAuthHeader())
+      this.dataService.httpPostMethod('/api/profiles/public/modifyProfileData', profileFormData, this.dataService.getAuthHeader())
     ]).subscribe(res => {
+      this.cvChanging = false;
       if (res[0].error || res[1].error) {
         this.dialog.open(MessageDialogComponent, {
           data: { icon: 'warning', text: 'asd' },

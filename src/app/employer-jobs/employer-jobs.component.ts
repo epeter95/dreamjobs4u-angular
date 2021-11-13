836118248a996b2ddcdb65f0e818c5ec7c4dce57
Péter Erdősi <peter.erdosi@extremalelement.com>
2021-11-13 08:02:56 +0100

    added public contents to epmloyer jobs

--- src/app/employer-jobs/employer-jobs.component.ts
+++ src/app/employer-jobs/employer-jobs.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PublicContent } from '../interfaces/public-contents';
+import { DataService } from '../services/data.service';
+import { LanguageService } from '../services/language.service';
 
 @Component({
   selector: 'app-employer-jobs',
   templateUrl: './employer-jobs.component.html',
   styleUrls: ['./employer-jobs.component.scss']
 })
-export class EmployerJobsComponent implements OnInit {
+export class EmployerJobsComponent implements OnInit, OnDestroy {
+  pageLoaded!: Promise<boolean>;
+  languageSubscription: Subscription = new Subscription();
+  publicContents: PublicContent[] = new Array();
+  myJobsTitleText: string = '';
+  creatingJobTitleText: string = '';
+  modifyJobTitleText: string = '';
 
-  constructor() { }
+  constructor(private dataService: DataService, private languageService: LanguageService) { }
 
   ngOnInit(): void {
+    this.dataService.getAllData('/api/publicContents/getByPagePlaceKey/employerJobs/public').subscribe(res=>{
+      this.publicContents = res;
+      this.languageSubscription = this.languageService.languageObservable$.subscribe(lang => {
+        if(lang){
+          this.myJobsTitleText = this.languageService.getTranslationByKey(lang, this.publicContents, 'title', 'myJobsTitleText', 'PublicContentTranslations');
+          this.creatingJobTitleText = this.languageService.getTranslationByKey(lang, this.publicContents, 'title', 'createJobTitle', 'PublicContentTranslations');
+          this.modifyJobTitleText = this.languageService.getTranslationByKey(lang, this.publicContents, 'title', 'modifyJobTitle', 'PublicContentTranslations');
+          this.pageLoaded = Promise.resolve(true);
+        }
+      });
+    });
+  }
+
+  ngOnDestroy(){
+    this.languageSubscription.unsubscribe();
   }
 
 }

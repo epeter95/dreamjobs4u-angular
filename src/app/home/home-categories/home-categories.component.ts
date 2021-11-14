98e64cf0a95c184d0126c51a7196362747c777e2
Péter Erdősi <peter.erdosi@extremalelement.com>
2021-11-14 23:55:41 +0100

    home categroies tablet and mobile view are implemented

--- src/app/home/home-categories/home-categories.component.ts
+++ src/app/home/home-categories/home-categories.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { Category } from 'src/app/interfaces/category';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-home-categories',
@@ -12,7 +13,27 @@ export class HomeCategoriesComponent implements OnInit {
   @Input() categorySubtitle: string = '';
   @Input() categoryJobCountText: string = '';
   @Input() allCategoryButtonText: string = '';
-  constructor() { }
+  isMobile: boolean = false;
+  sliceIndex: number = 8;
+  constructor(private dataService: DataService) {
+    this.calculateSliceIndex();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.calculateSliceIndex();
+  }
+
+  calculateSliceIndex(){
+    let windowWidth = window.innerWidth;
+    if(window.innerWidth >= this.dataService.mobileWidth){
+      this.sliceIndex = 8;
+    } else if(windowWidth<this.dataService.mobileWidth && windowWidth >= 900){
+      this.sliceIndex = 6;
+    } else if(windowWidth<900){
+      this.sliceIndex = 4;
+    }
+  }
 
   ngOnInit(): void {
   }

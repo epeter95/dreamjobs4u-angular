b287bcdb01e0df14767353485575848a96f119cc
Péter Erdősi <peter.erdosi@extremalelement.com>
2021-11-19 10:40:56 +0100

    added production api domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiDomain: 'http://localhost:3000',
+  apiDomain: 'https://sweetjobs-backend.herokuapp.com',
   employerRoles: ['rm5oss9j6OhqpnRwBfWM','xos9L09D3OYFtYWDXdj2','7OEHVkA0mjeE53xl6qYh'],
   employeeRoles: ['g5SB8qaKNdj8K0Gw3WzK','xos9L09D3OYFtYWDXdj2','7OEHVkA0mjeE53xl6qYh'],
   production: true

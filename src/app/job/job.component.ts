52cc099728fc5a7f25b0f210103d9c348449386c
Péter Erdősi <peter.erdosi@extremalelement.com>
2021-11-23 19:43:17 +0100

    fixed domain

--- src/app/job/job.component.ts
+++ src/app/job/job.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
 import { forkJoin, Subscription } from 'rxjs';
 import { ErrorMessage } from '../interfaces/error-message';
 import { GeneralMessage } from '../interfaces/general-message';
@@ -51,7 +52,7 @@ export class JobComponent implements OnInit, OnDestroy {
     this.isEmployerRole = this.roleService.checkEmployerRole(this.roleService.getRole()!);
     this.activatedRoute.paramMap.subscribe(params => {
       let id = params.get('jobId');
-      this.fbUrl = 'https://www.facebook.com/plugins/share_button.php?href=' + encodeURIComponent(window.location.origin + '/api/facebook/' + id) + '&layout=button_count&size=small&width=100&height=20&appId';
+      this.fbUrl = 'https://www.facebook.com/plugins/share_button.php?href=' + encodeURIComponent(environment.apiDomain + '/api/facebook/' + id) + '&layout=button_count&size=small&width=100&height=20&appId';
       forkJoin([
         this.dataService.getOneData('/api/jobs/public/getJobById/' + id),
         this.dataService.getAllData('/api/publicContents/getByPagePlaceKey/jobPage/public'),

718a67e43eb43e55cf22ffcc9a462c4c56ef5471
Péter Erdősi <peter.erdosi@extremalelement.com>
2021-11-30 09:52:40 +0100

    category component functionality is implemented into jobs component

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { forkJoin, Observable, Subscription } from 'rxjs';
@@ -16,7 +16,7 @@ import { SessionService } from '../services/session.service';
   templateUrl: './jobs.component.html',
   styleUrls: ['./jobs.component.scss']
 })
-export class JobsComponent implements OnInit {
+export class JobsComponent implements OnInit, OnDestroy {
   jobs: Job[] = new Array();
   filteredJobs: Job[] = new Array();
   jobsTitleText: string = '';
@@ -24,11 +24,13 @@ export class JobsComponent implements OnInit {
   searchButtonText: string = '';
   publicContents: PublicContent[] = new Array();
   languageSubscription: Subscription = new Subscription();
+  activatedRouteSubscription: Subscription = new Subscription();
   isCategoryDropdownOpen: boolean = false;
   pageLoaded!: Promise<boolean>;
   onFirstLoad: boolean = true;
   fromSearch: boolean = false;
   isUserLoggedIn: boolean = false;
+  jobCountInSelectedCategory: number = 0;
 
   searchTermElement: FormElement = {
     key: 'jobTextSearchTerm', placeholder: '', focus: false,
@@ -44,6 +46,13 @@ export class JobsComponent implements OnInit {
     jobCategorySearchTerm: new FormControl(''),
   });
 
+  @HostListener('window:popstate', ['$event'])
+  onPopState(event: any) {
+    this.languageSubscription.unsubscribe();
+    this.activatedRouteSubscription.unsubscribe();
+    this.initData();
+  }
+
 
   constructor(private dataService: DataService,
     private languageService: LanguageService,
@@ -56,6 +65,15 @@ export class JobsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.initData();
+  }
+
+  ngOnDestroy(){
+    this.languageSubscription.unsubscribe();
+    this.activatedRouteSubscription.unsubscribe();
+  }
+
+  initData() {
     let sources: Observable<any>[] = [
       this.dataService.getAllData('/api/jobs/public'),
       this.dataService.getAllData('/api/publicContents/getByPagePlaceKey/jobsPage/public'),
@@ -70,8 +88,10 @@ export class JobsComponent implements OnInit {
       this.jobs = res[0];
       this.publicContents = res[1];
       this.categories = res[2];
+      this.categories.map(element=>{
+        this.jobCountInSelectedCategory+=element.Jobs.length;
+      })
       if (res[3]) {
-        console.log(res[3]);
         this.preferredCategories = res[3].Categories;
         if (this.preferredCategories.length > 0) {
           this.jobs = this.jobs.sort((a, b) => {
@@ -81,7 +101,7 @@ export class JobsComponent implements OnInit {
           });
         }
       }
-      this.activatedRoute.queryParamMap.subscribe(params => {
+      this.activatedRouteSubscription = this.activatedRoute.queryParamMap.subscribe(params => {
         if (params.get('text')) {
           this.searchForm.controls.jobTextSearchTerm.setValue(params.get('text'))
         }
@@ -89,6 +109,14 @@ export class JobsComponent implements OnInit {
           let selectedCategory = this.categories.find(element => element.id == +params.get('category')!)!;
           selectedCategory.selectedTranslation = this.languageService.getTranslation(this.languageService.getLangauge()!, selectedCategory.CategoryTranslations);
           this.searchForm.controls.jobCategorySearchTerm.setValue(selectedCategory.selectedTranslation.text);
+          this.jobCountInSelectedCategory = selectedCategory.Jobs.length;
+        }else{
+          let selectedCategory = this.categories.find(element => element.key == 'allCategory')!;
+          selectedCategory.selectedTranslation = this.languageService.getTranslation(this.languageService.getLangauge()!, selectedCategory.CategoryTranslations);
+          this.searchForm.controls.jobCategorySearchTerm.setValue(selectedCategory.selectedTranslation.text);
+          this.categories.map(element=>{
+            this.jobCountInSelectedCategory+=element.Jobs.length;
+          });
         }
       })
       this.languageSubscription = this.languageService.languageObservable$.subscribe(lang => {
@@ -108,7 +136,7 @@ export class JobsComponent implements OnInit {
         });
         this.filteredJobs = this.jobs;
         if (this.searchForm.controls.jobTextSearchTerm.value || this.searchForm.controls.jobCategorySearchTerm.value) {
-          this.filterJobs();
+          this.filterJobs(false);
         }
         this.searchTermElement.placeholder = this.languageService.getTranslationByKey(lang, this.publicContents, 'title', 'jobTextSearchTerm', 'PublicContentTranslations');
         this.categoriesDropDown.placeholder = this.languageService.getTranslationByKey(lang, this.publicContents, 'title', 'jobCategorySearchTerm', 'PublicContentTranslations');
@@ -120,10 +148,6 @@ export class JobsComponent implements OnInit {
     });
   }
 
-  initData() {
-
-  }
-
   setSelectedCategory(category: Category) {
     this.searchForm.controls.jobCategorySearchTerm.setValue(category.selectedTranslation.text);
     this.isCategoryDropdownOpen = false;
@@ -133,12 +157,26 @@ export class JobsComponent implements OnInit {
     this.isCategoryDropdownOpen = !this.isCategoryDropdownOpen;
   }
 
-  filterJobs() {
+  filterJobs(navigationNeeded: boolean) {
     const searchTerm = this.searchForm.controls.jobTextSearchTerm.value;
     const jobCategorySearchTerm = this.searchForm.controls.jobCategorySearchTerm.value;
+    let queryParams: any = {};
     this.filteredJobs = this.jobs;
     if (searchTerm || jobCategorySearchTerm) {
+      if(searchTerm){
+        queryParams['text'] = searchTerm;
+      }
       const category = this.categories.find(element => element.selectedTranslation.text == jobCategorySearchTerm);
+      if(jobCategorySearchTerm){
+        queryParams['category'] = category!.id;
+      }
+      if(category!.key == 'allCategory'){
+        this.categories.map(element=>{
+          this.jobCountInSelectedCategory+=element.Jobs.length;
+        })
+      }else{
+        this.jobCountInSelectedCategory = category!.Jobs.length;
+      }
       this.filteredJobs = this.jobs.filter(element => {
         if (category && category.key != 'allCategory') {
           return (element.categoryId == category?.id && JSON.stringify(element.JobTranslations).toLowerCase().includes(searchTerm));
@@ -146,7 +184,9 @@ export class JobsComponent implements OnInit {
         return (JSON.stringify(element.JobTranslations).toLowerCase().includes(searchTerm));
       });
     }
-    this.router.navigate(['/allasok']);
+    if(navigationNeeded){
+      this.router.navigate(['/allasok'], {queryParams: queryParams ? queryParams : null});
+    }
   }
 
 }
